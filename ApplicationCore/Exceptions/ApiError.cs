fde53ae4e9384d36871cfb01ae6dd3504d36224c
Er_Jimmy goel <er_jimmy_goel@hotmail.com>
2022-10-26 12:35:54 +0530

    Section 7

---
+++ ApplicationCore/Exceptions/ApiError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Exceptions
+{
+    public class ApiError
+    {
+        public ApiError(int statuscode, string messages = null, string details = null)
+        {
+            this.StatusCode = statuscode;
+            this.Messages = messages;
+            this.Details = details;
+        }
+
+        public int StatusCode { get; set; }
+        public string Messages { get; set; }
+        public string Details { get; set; }
+
+    }
+}
--- PublicApi/Startup.cs
+++ PublicApi/Startup.cs
@@ -37,11 +37,11 @@ namespace PublicApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseMiddleware<ExecptionMiddleware>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
